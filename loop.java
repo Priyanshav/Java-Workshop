8601e143cbab26b4d9fd40f6e8c8675411fa454f
Priyanshav <priyanshavkmr6@gmail.com>
2024-09-18 16:18:33 +0530

    loops

---
+++ loop.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class loop{
+
+    // public static void natural(int n){
+    //     int j,sum=0;
+    //     for(j=n;j>0;j--){
+    //         sum = sum+j;
+    //     }
+    //     System.out.println("Sum = "+sum);
+    // }
+
+    // public static void even(int n){
+    //     int j,sum=0;
+    //     for(j=0;j<=n;j=j+2){
+    //         sum = sum+j;
+    //     }
+    //     System.out.println("Sum = "+sum);
+    // }
+
+    // public static void odd(int n,int m){
+    //     int j,sum=0;
+    //     for(j=n;j<=m;j++){
+    //         if(j%2!=0){
+    //             sum = sum+j;
+    //         }
+    //     }
+    //     System.out.println("Sum of odd numbers: " +sum);
+    // }
+
+    // public static void table(int n){
+    //     int j,mul;
+    //     for(j=1;j<11;j++){
+    //         mul = j*n;
+    //         System.out.println(mul);
+    //     }
+    // }
+
+    // public static void threemultiples(int n){
+    //     int j,sum=0,mul=3;
+    //     for(j=1;j<n;j+=3){
+    //         mul = mul*j;
+    //         sum = sum+mul;
+    //     }
+    //     System.out.println("sum = "+sum);
+    // }
+
+    public static void main(String[] args){
+        int num,num2;
+        Scanner i = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = i.nextInt();
+        // num2 = i.nextInt();
+        // natural(num);
+        // even(num);
+        // odd(num,num2);
+        // table(num);
+        // threemultiples(num);
+    }
+}
